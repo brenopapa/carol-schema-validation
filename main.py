035485ea134f5d4656aa7cab10b5c8f329e036d3
Breno Papa <bpapa@outlook.com.br>
2023-10-11 16:47:23 -0300

    Adding more context to output

--- main.py
+++ main.py
@@ -6,20 +6,35 @@ args = vars(PARSER.parse_args())
 
 def run():
 
+
     default = carol_auth.tenant_login(args.get("default"))
     target = carol_auth.tenant_login(args.get("target"))
     
     #BQ COMPARISON
-    default_bq_schema = schema.bigquery_schema(default, args.get("connector"))
-    target_bq_schema = schema.bigquery_schema(target, args.get("connector"))
-    
-    print(schema.bigquery_compare(default_bq_schema, target_bq_schema))
-
+    try:
+        print(f"Getting BQ schemas for {default} and {target}... \n")
+        default_bq_schema = schema.bigquery_schema(default, args.get("connector"))
+        target_bq_schema = schema.bigquery_schema(target, args.get("connector"))
+        print('Success!!! \n')
+    except Exception as e:
+        raise(e)
+    try:
+        print(f"BQ Fields on {default} not present in {target}: \n")
+        print(schema.bigquery_compare(default_bq_schema, target_bq_schema))
+    except Exception as e:
+        raise(e)
     #CAROL COMPARISON
-    default_carol_schema = schema.carol_schema(default, args.get("connector"))
-    target_carol_schema = schema.carol_schema(target, args.get("connector"))
-
-    print(schema.carol_compare(default_carol_schema, target_carol_schema))
-
+    try:
+        print(f"Getting CAROL PKs (crosswalks) for {default} and {target}... \n")
+        default_carol_schema = schema.carol_schema(default, args.get("connector"))
+        target_carol_schema = schema.carol_schema(target, args.get("connector"))
+        print('Success!!! \n')
+    except Exception as e:
+        raise(e)
+    try:
+        print(f"Divergent PKs (crosswalks): \n")
+        print(schema.carol_compare(default_carol_schema, target_carol_schema))
+    except Exception as e:
+        raise(e)
 if __name__ == "__main__":
     run()
